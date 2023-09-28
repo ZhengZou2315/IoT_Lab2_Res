f1eee9e700467bb19e02e5f0035c34a70f59070b
zhengzou <zheng.zou3459@gmail.com>
2023-09-27 21:06:58 -0700

    update

---
+++ pi_socket.py
@@ -0,0 +1,163 @@
+import socket
+import threading
+from collections import deque
+import signal
+import time
+import picar_4wd as fc
+
+server_addr = 'D8:3A:DD:21:3F:0E'
+server_port = 1
+
+def turn_left():
+  # turn 90 degree
+  # turn left parameters
+  fc.turn_left(20)
+  time.sleep(1.35)
+  fc.stop()
+
+def turn_right():
+  # turn 90 degree
+  # turn right parameters
+  fc.turn_right(20)
+  time.sleep(1.20)
+  fc.stop()
+
+def move_forward(x:int):
+  time_interval = 0.1
+  speed_val = 25
+  speed4 = fc.Speed(speed_val)
+  speed4.start()
+  dist = 0
+  speed = -1
+  fc.forward(speed_val)
+  target_time = x/float(speed_val)
+  # interval_count = int(target_time / time_interval) + 1 if int(target_time / time_interval) > 0 else 0
+  interval_count = int(target_time*0.4 / time_interval)
+  print('target_time: ',target_time,'  interval count:', interval_count)
+  
+  for _ in range(interval_count):
+    time.sleep(time_interval)
+    speed = speed4()
+    dist += speed * time_interval
+    print("%scm/s"%speed)
+
+  speed4.deinit()
+  print('target dist: ',x, ' actual distance:  ',dist)
+  # speed4.deinit()
+  fc.stop()
+  return speed,round(dist,2)
+
+buf_size = 1024
+
+client_sock = None
+server_sock = None
+sock = None
+
+exit_event = threading.Event()
+
+message_queue = deque([])
+output = ""
+
+dq_lock = threading.Lock()
+output_lock = threading.Lock()
+
+def handler(signum, frame):
+    exit_event.set()
+
+signal.signal(signal.SIGINT, handler)
+
+def start_client():
+    global server_addr
+    global server_port
+    global server_sock
+    global sock
+    global exit_event
+    global message_queue
+    global output
+    global dq_lock
+    global output_lock
+    server_sock = socket.socket(socket.AF_BLUETOOTH, socket.SOCK_STREAM, socket.BTPROTO_RFCOMM)
+    server_sock.bind((server_addr, server_port))
+    server_sock.listen(1)
+    server_sock.settimeout(10000)
+    sock, address = server_sock.accept()
+    print("Connected")
+    server_sock.settimeout(None)
+    sock.setblocking(0)
+    target_dist = 20
+    dirs = ['N','E','S','W']
+    cur_idx = 0
+    while not exit_event.is_set():
+        if dq_lock.acquire(blocking=False):
+            if(len(message_queue) > 0):
+                try:
+                    sent = sock.send(bytes(message_queue[0], 'utf-8'))
+                except Exception as e:
+                    exit_event.set()
+                    continue
+                if sent < len(message_queue[0]):
+                    message_queue[0] = message_queue[0][sent:]
+                else:
+                    message_queue.popleft()
+            dq_lock.release()
+        
+        if output_lock.acquire(blocking=False):
+            data = ""
+            try:
+                try:
+                    data = sock.recv(1024).decode('utf-8')
+                    print('data:',data,' len(data):',len(data))
+                    if data == 'up':
+                        speed,dist = move_forward(target_dist)
+                    elif data == 'left':
+                        print('In the left mode!!!!')
+                        turn_left()
+                        cur_idx = (cur_idx+3) % len(dirs)
+                        speed,dist = move_forward(target_dist)
+                    elif data == 'right':
+                        turn_right()
+                        cur_idx = (cur_idx+1) % len(dirs)
+                        speed,dist = move_forward(target_dist)
+                    elif data == 'down':
+                        turn_left()
+                        turn_left()
+                        cur_idx = (cur_idx+3) % len(dirs)
+                        cur_idx = (cur_idx+3) % len(dirs)
+                        speed,dist = move_forward(target_dist)
+                    response = 'speed:{speed}, distance:{dist}'.format(speed=speed,dist=dist)
+                    message_queue.append(response)
+                except socket.error as e:
+                    assert(1==1)
+                    #no data
+
+            except Exception as e:
+                exit_event.set()
+                continue
+            output += data
+            output_split = output.split("\r\n")
+            for i in range(len(output_split) - 1):
+                print(output_split[i])
+            output = output_split[-1]
+            output_lock.release()
+    server_sock.close()
+    sock.close()
+    print("client thread end")
+
+
+cth = threading.Thread(target=start_client)
+
+cth.start()
+
+j = 0
+while not exit_event.is_set():
+    dq_lock.acquire()
+    # message_queue.append("RPi " + str(j) + " \r\n")
+    dq_lock.release()
+    j += 1
+    time.sleep(1.5)
+    
+
+print("Disconnected.")
+
+
+print("All done.")
---
+++ wifi_server.py
@@ -0,0 +1,96 @@
+import socket
+import picar_4wd as fc
+import time, math
+
+HOST = "192.168.1.209" # IP address of your Raspberry PI
+PORT = 65432          # Port to listen on (non-privileged ports are > 1023)
+
+def turn_left():
+  # turn 90 degree
+  # turn left parameters
+  fc.turn_left(20)
+  time.sleep(1.35)
+  fc.stop()
+
+def turn_right():
+  # turn 90 degree
+  # turn right parameters
+  fc.turn_right(20)
+  time.sleep(1.20)
+  fc.stop()
+
+def move_forward(x:int):
+  time_interval = 0.1
+  speed_val = 25
+  speed4 = fc.Speed(speed_val)
+  speed4.start()
+  dist = 0
+  speed = -1
+  fc.forward(speed_val)
+  target_time = x/float(speed_val)
+  # interval_count = int(target_time / time_interval) + 1 if int(target_time / time_interval) > 0 else 0
+  interval_count = int(target_time*0.4 / time_interval)
+  print('target_time: ',target_time,'  interval count:', interval_count)
+  
+  for _ in range(interval_count):
+    time.sleep(time_interval)
+    speed = speed4()
+    dist += speed * time_interval
+    print("%scm/s"%speed)
+
+  speed4.deinit()
+  print('target dist: ',x, ' actual distance:  ',dist)
+  # speed4.deinit()
+  fc.stop()
+  return speed,round(dist,2)
+
+
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.bind((HOST, PORT))
+    s.listen()
+    dirs = ['N','E','S','W']
+    cur_idx = 0
+    speed = -1
+    dist = -1
+    target_dist = 20
+    try:
+      while 1:
+        client, clientInfo = s.accept()
+        print("server recv from: ", clientInfo)
+        data = client.recv(1024)      # receive 1024 Bytes of message in binary format
+        # if data != b"":
+        command = data.decode('ascii')[:-2]
+        # print('data:',data,'  command:',command,' type(command):',type(command), 'len: ',len(command))
+        # for ch in list(command):
+        #    print('ch: ',ch,' \n')
+        if command == 'up':
+            speed,dist = move_forward(target_dist)
+        elif command == 'left':
+            print('In the left mode!!!!')
+            turn_left()
+            cur_idx = (cur_idx+3) % len(dirs)
+            speed,dist = move_forward(target_dist)
+        elif command == 'right':
+            turn_right()
+            cur_idx = (cur_idx+1) % len(dirs)
+            speed,dist = move_forward(target_dist)
+        elif command == 'down':
+            turn_left()
+            turn_left()
+            cur_idx = (cur_idx+3) % len(dirs)
+            cur_idx = (cur_idx+3) % len(dirs)
+            speed,dist = move_forward(target_dist)
+        else:
+           client.sendall(data)
+        print('speed: ',speed,'  dist: ',dist)
+        response = ','.join([dirs[cur_idx],str(speed),str(dist)])     
+        data = response.encode('ascii')
+        client.sendall(data) # Echo back to client
+    except Exception as e: 
+      print('e: ',e)
+      print("Closing socket")
+      client.close()
+      s.close()    
+
+# netstat -anpe | grep "65432"
